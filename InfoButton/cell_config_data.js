4f9385f1e8a6817d4799b838dae5ebb79d8e23e8
tikenn <tikennprogrammer@gmail.com>
2015-10-24 16:30:35 -0500

    Added basic plugin build in

---
+++ InfoButton/cell_config_data.js
@@ -0,0 +1,40 @@
+/**
+ * This file is a part of the I2B2 InfoButtonPlugin
+ * Copyright (C) 2015  Tim Kennell Jr. and James Cimino, MD
+ * one line to give the program's name and an idea of what it does.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ **
+ * Loads file dependencies for InfoButton plugin
+ **/
+
+{
+    "files": ["InfoButton_ctrlr.js"],
+    "css": ["InfoButton.css"],
+    "config": {
+        "short_name": "InfoButton Plugin",
+        "name": "InfoButton Plugin -- basic build to get it working",
+        "description": "Plugin built to retrieve information from OpenInfoButton in an HL7 compliant standard",
+        "category": ["celless", "plugin", "standard"],
+        "plugin": {
+            "isolateHtml": false,
+            "isolateComm": false,
+            "standardTabs": true,
+            "html": {
+                "source": "injected_screens.html",
+                "mainDivId": "InfoButton-div"
+            }
+        }
+    }
+}
